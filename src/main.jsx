ce5dc435ee44ef03a88eb6affe88a05077cfbeb2
Ashikqul <ashikqulislamrizvi77@gmail.com>
2025-08-09 00:53:38 +0600

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
-import {RouterProvider,} from "react-router";
+import {RouterProvider} from "react-router";
 import { router } from './router/router.jsx';
+
+import Snowfall from 'react-snowfall';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div className="bg-[#0E0B2E] min-h-screen">
+    <div className="bg-[#0E0B2E] min-h-screen relative">
+      <Snowfall />
       <RouterProvider router={router} />
     </div>
   </StrictMode>,
